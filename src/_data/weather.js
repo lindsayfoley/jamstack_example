219e71c7cb388068222db2e3306d493de78656c5
Lindsay Foley <lindsay.foley@outlook.com>
2020-11-18 00:08:33 +0000

    moving if

--- src/_data/weather.js
+++ src/_data/weather.js
@@ -1,22 +1,22 @@
 const success = (position) => {
-
   const lat = position.coords.latitude;
   const long = position.coords.longitude;
 
   const url = `/.netlify/functions/weatherapi?lat=${lat}&lon=${long}&units=metric`;
 
   fetch(url)
-    .then(response => response.json())
-      .then(data => {
-        console.log('data', data);
-        document.querySelector('.region').textContent = data.name;
-        document.querySelector('.summary').textContent = data.current.condition.text;
-        document.querySelector('.temp').textContent = data.current.temp_c;
-        document.querySelector('.icon').textContent = data.current.condition.icon;
-        document.querySelector('.weather').classList.remove('hide');
-      });
-};
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data", data);
+      document.querySelector(".region").textContent = data.name;
+      document.querySelector(".summary").textContent =
+        data.current.condition.text;
+      document.querySelector(".temp").textContent = data.current.temp_c;
+      document.querySelector(".icon").textContent = data.current.condition.icon;
+      document.querySelector(".weather").classList.remove("hide");
+    });
 
-if(navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success, error);
-}
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+};
