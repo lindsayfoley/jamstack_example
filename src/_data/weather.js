9bf4978f3a7e7a0a3c18e587dc611896fb2d5b0b
Lindsay Foley <lindsay.foley@outlook.com>
2020-11-18 00:07:00 +0000

    correcting typos

--- src/_data/weather.js
+++ src/_data/weather.js
@@ -1,21 +1,22 @@
 const success = (position) => {
+
   const lat = position.coords.latitude;
   const long = position.coords.longitude;
 
-  const url = `/.netflify/functions/weatherapi/?lat=${lat}&lon=${long}`;
+  const url = `/.netlify/functions/weatherapi?lat=${lat}&lon=${long}&units=metric`;
 
   fetch(url)
-    .then((response) => response.json())
-    .then(data => {
-      document.querySelector('region').append(data.location.region);
-      document.querySelector('summary').append(data.current.condition.text);
-      document.querySelector('temp').append(data.current.temp_c);
-      document.querySelector('icon').append(data.current.condition.icon);
-      document.querySelector('icon').append(data.current.condition.icon);
-      document.getElementById('weather').classList.remove('hide');
-    });
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
+    .then(response => response.json())
+      .then(data => {
+        console.log('data', data);
+        document.querySelector('.region').textContent = data.name;
+        document.querySelector('.summary').textContent = data.current.condition.text;
+        document.querySelector('.temp').textContent = data.current.temp_c;
+        document.querySelector('.icon').textContent = data.current.condition.icon;
+        document.querySelector('.weather').classList.remove('hide');
+      });
 };
+
+if(navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -1,12 +1,14 @@
 const { default: Axios } = require("axios");
 
-exports.handler = async function (event, context, callback) {
-  const { lat, long } = event.queryStringParameters;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.WEATHER_API_KEY}`;
+exports.handler = async function(event, context, callback) {
 
-  const response = await Axios.get(url);
+  const { lat, long } = event.queryStringParameters;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.WEATHER_API_KEY}`;
+
+  const response = await axios.get(url);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(response.data)
   });
 };
+  
