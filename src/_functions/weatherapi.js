1c534f9eb22c5bdb6bf70b22c33325e9e85b1396
Lindsay Foley <lindsay.foley@outlook.com>
2022-05-04 23:07:52 +0100

    testing lat long

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -2,17 +2,21 @@ const axios = require("axios");
 require("dotenv").config();
 
 exports.handler = async function (event, context, callback) {
-  const getUsersLatLong = navigator.geolocation
-    ? navigator.geolocation.getCurrentPosition(showPosition)
+  const getUsersGeoLocation = navigator.geolocation
+    ? navigator.geolocation.getCurrentPosition()
     : undefined;
 
-  const usersLatLong = getUsersLatLong();
+  const usersGeoLocation = getUsersGeoLocation();
 
   if (!usersLatLong) {
     return null;
   }
 
-  const weatherApi = `https://api.weatherapi.com/v1/current.json?q=${usersLatLong}&key=${process.env.WEATHER_API_KEY}`;
+  const lat = usersGeoLocation.coords.latitude;
+  const long = sersGeoLocation.coords.longitude;
+  console.log("lat", lat, "long", long);
+
+  const weatherApi = `https://api.weatherapi.com/v1/current.json?q=${lat},${long}&key=${process.env.WEATHER_API_KEY}`;
 
   const response = await axios.get(weatherApi);
 
